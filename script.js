7959bfee807fcbdcd898ff438f644f0b56448052
Michael Cohen <shplishka@gmail.com>
2024-01-29 15:24:03 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const text = document.querySelector('.animated-text');
+    setTimeout(() => {
+        text.style.color = '#ff4500';
+    }, 2000);
+});
